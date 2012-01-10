4e26ef1f3d2d36c72ea24acc52fa127bdcdd3896
Benoît SIMARD <github@bsimard.com>
2012-01-10 13:48:29 +0100

    correct shutting down

--- src/play/modules/neo4j/util/Neo4j.java
+++ src/play/modules/neo4j/util/Neo4j.java
@@ -54,7 +54,8 @@ public class Neo4j {
                 bootstrapperDb.remove();
             }
         }
-        db().shutdown();
+        graphDb.get().shutdown();
+        graphDb.remove();
     }
 
     /**
