559e9ac695a7b9ec32785d5919d09fb56fb6577f
Benoît SIMARD <github@bsimard.com>
2012-01-15 17:04:04 +0100

    refactoring after merging

--- src/play/modules/neo4j/model/Neo4jModelEnhancer.java
+++ src/play/modules/neo4j/model/Neo4jModelEnhancer.java
@@ -1,69 +1,40 @@
 package play.modules.neo4j.model;
 
-import javassist.*;
-import play.Logger;
-import play.classloading.ApplicationClasses.ApplicationClass;
-import play.exceptions.UnexpectedException;
-
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Modifier;
+
+import javassist.CtClass;
+import javassist.CtConstructor;
+import javassist.CtField;
+import javassist.CtMethod;
+import javassist.CtNewConstructor;
+import javassist.NotFoundException;
+import play.Logger;
+import play.classloading.ApplicationClasses.ApplicationClass;
+import play.classloading.enhancers.Enhancer;
+import play.exceptions.UnexpectedException;
 
 /**
  * Enhance <code>Neo4jModel</code> to add getter & setter wich are delegate operation to the underlying node (@see
  * Neo4jModel.class).
- *
+ * 
  * @author bsimard
+ * 
  */
-public class Neo4jModelEnhancer extends Neo4jEnhancer {
+public class Neo4jModelEnhancer extends Enhancer {
 
     @Override
     public void enhanceThisClass(ApplicationClass applicationClass) throws Exception {
         CtClass ctClass = makeClass(applicationClass);
         String entityName = ctClass.getName();
+        Logger.debug("Enhance class " + entityName);
 
         // Only enhance Neo4jModel classes.
         if (!ctClass.subtypeOf(classPool.get("play.modules.neo4j.model.Neo4jModel"))) {
             return;
         }
-        Logger.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        Logger.debug("Enhance Neo4jModel class " + entityName);
 
         // Add a default constructor if needed
-        addDefaultConstructor(ctClass);
-
-        // for all field, we add getter / setter
-        for (CtField ctField : ctClass.getDeclaredFields()) {
-
-            try {
-                String propertyName = ctField.getName().substring(0, 1).toUpperCase()
-                        + ctField.getName().substring(1);
-                if (isProperty(ctField)) {
-                    Logger.debug("##### Field " + ctField.getName() + " is a property");
-                    getterMethod(ctClass, entityName, ctField, "get" + propertyName);
-                    setterMethod(ctClass, entityName, ctField, "set" + propertyName);
-                } else if (isAnnotatedWithRelatedToVia(ctField)) {
-                    getterMethodForRelatedToVia(ctClass, entityName, ctField, "get" + propertyName);
-                } else {
-                    Logger.debug("##### Field " + ctField.getName() + " skipped");
-                }
-            } catch (Exception e) {
-                Logger.error(e, "Error while adding Getter/Setter in Neo4jModelEnhancer");
-                throw new UnexpectedException("Error in PropertiesEnhancer", e);
-            }
-        }
-
-        getByKeyMethod(ctClass, entityName);
-        findAllMethod(ctClass, entityName);
-        cleanUpMethod(ctClass, entityName);
-
-        // Done.
-        applicationClass.enhancedByteCode = ctClass.toBytecode();
-        ctClass.defrost();
-        Logger.debug("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-
-    }
-
-    private void addDefaultConstructor(CtClass ctClass) {
         try {
             boolean hasDefaultConstructor = false;
             for (CtConstructor constructor : ctClass.getDeclaredConstructors()) {
@@ -81,21 +52,22 @@ public class Neo4jModelEnhancer extends Neo4jEnhancer {
             Logger.error(e, "Error in PropertiesEnhancer");
             throw new UnexpectedException("Error in PropertiesEnhancer", e);
         }
-    }
 
-<<<<<<< HEAD
         // for all field, we add getter / setter
         for (CtField ctField : ctClass.getDeclaredFields()) {
             try {
+                // Property name
+                String propertyName = ctField.getName().substring(0, 1).toUpperCase() + ctField.getName().substring(1);
+                String getter = "get" + propertyName;
+                String setter = "set" + propertyName;
+
                 Logger.debug("Field " + ctField.getName() + " is a property ?");
                 if (isProperty(ctField)) {
                     Logger.debug("true");
-                    // Property name
-                    String propertyName = ctField.getName().substring(0, 1).toUpperCase()
-                            + ctField.getName().substring(1);
-                    String getter = "get" + propertyName;
-                    String setter = "set" + propertyName;
 
+                    // ~~~~~~~~~
+                    // GETTER
+                    // ~~~~~~~
                     try {
                         CtMethod ctMethod = ctClass.getDeclaredMethod(getter);
                         if (!ctMethod.getName().equalsIgnoreCase("getShouldBeSave")) {
@@ -105,7 +77,7 @@ public class Neo4jModelEnhancer extends Neo4jEnhancer {
                     } catch (NotFoundException noGetter) {
 
                         // create getter
-                        Logger.debug("Adding getter  " + getter + " for class " + entityName);
+                        Logger.debug("Adding getter " + getter + " for class " + entityName);
                         //@formatter:off
                         String code = "public " + ctField.getType().getName() + " " + getter + "() {" +
                                             "if(this.shouldBeSave == Boolean.FALSE && this.node != null){" +
@@ -120,6 +92,9 @@ public class Neo4jModelEnhancer extends Neo4jEnhancer {
                         ctClass.addMethod(getMethod);
                     }
 
+                    // ~~~~~~~~~
+                    // SETTER
+                    // ~~~~~~~
                     try {
                         CtMethod ctMethod = ctClass.getDeclaredMethod(setter);
                         if (ctMethod.getParameterTypes().length != 1
@@ -133,133 +108,94 @@ public class Neo4jModelEnhancer extends Neo4jEnhancer {
                         }
                     } catch (NotFoundException noSetter) {
                         // create setter
-                        Logger.debug("Adding setter  " + getter + " for class " + entityName);
+                        Logger.debug("Adding setter " + getter + " for class " + entityName);
                         //@formatter:off
                         CtMethod setMethod = CtMethod
                                 .make("public void " + setter + "(" + ctField.getType().getName() + " value) { " +
-                                            "this."  + ctField.getName() + " = value;" +
+                                            "this." + ctField.getName() + " = value;" +
                                             "this.shouldBeSave = Boolean.TRUE;" +
                                       "}", ctClass);
                         //formatter:on
                         ctClass.addMethod(setMethod);
                     }
-=======
-    private void setterMethod(CtClass ctClass, String entityName, CtField ctField, String setter) throws NotFoundException, CannotCompileException {
-        try {
-            CtMethod ctMethod = ctClass.getDeclaredMethod(setter);
-            if (ctMethod.getParameterTypes().length != 1
-                    || !ctMethod.getParameterTypes()[0].equals(ctField.getType())
-                    || Modifier.isStatic(ctMethod.getModifiers())
-                    || hasPlayPropertiesAccessorAnnotation(ctMethod)) {
-                if (hasPlayPropertiesAccessorAnnotation(ctMethod)) {
-                    ctClass.removeMethod(ctMethod);
->>>>>>> aa293e3863d449c8622672a2fc1730b89627ac5e
                 }
-                throw new NotFoundException("it's not a true setter !");
+                else{
+                    // ~~~~~~~~~
+                    // GETTER for neo4j relation property 
+                    // ~~~~~~~
+                    if(hasNeo4jRelatedAnnotation(ctField)){
+                        //@formatter:off
+                        String code = "public " + ctField.getType().getName() + " " + getter + "() {" +
+                                            "if(this." + ctField.getName() + " == null){" +
+                                                "return getIterator(\"" + entityName + "\");" +
+                                            "}else{" +
+                                                "return " + ctField.getName() + ";" +
+                                            "}" +
+                                      "}";
+                        //@formatter:on
+                        Logger.debug(code);
+                        CtMethod method = CtMethod.make(code, ctClass);
+                        ctClass.addMethod(method);
+                    }
+                }
+            } catch (Exception e) {
+                Logger.error(e, "Error in PropertiesEnhancer");
+                throw new UnexpectedException("Error in PropertiesEnhancer", e);
             }
-        } catch (NotFoundException noSetter) {
-            String code = "public void " + setter + "(" + ctField.getType().getName() + " value) { " +
-                    "this." + ctField.getName() + " = value;" +
-                    "this.shouldBeSave = Boolean.TRUE;" +
-                    "}";
-            addMethod(ctClass, entityName, code, setter);
         }
-    }
 
-    private void getterMethod(CtClass ctClass, String entityName, CtField ctField, String getter) throws NotFoundException, CannotCompileException {
-        try {
-            CtMethod ctMethod = ctClass.getDeclaredMethod(getter);
-            if (!ctMethod.getName().equalsIgnoreCase("getShouldBeSave")) {
-                ctClass.removeMethod(ctMethod);
-                throw new NotFoundException("it's not a true getter !");
-            }
-        } catch (NotFoundException noGetter) {
+        // Adding getByKey() method
+        Logger.debug("Adding getByKey() method for class " + entityName);
+        //@formatter:off
+        String codeGetByKey = "public static play.modules.neo4j.model.Neo4jModel getByKey(Long key) throws play.modules.neo4j.exception.Neo4jException {" +
+                                    "return (" + entityName + ")_getByKey(key, \"" + entityName + "\");" +
+                                "}";
+        //@formatter:on
+        Logger.debug(codeGetByKey);
+        CtMethod getByKeyMethod = CtMethod.make(codeGetByKey, ctClass);
+        ctClass.addMethod(getByKeyMethod);
 
-            String code = "public " + ctField.getType().getName() + " " + getter + "() {" +
-                    "if(this.shouldBeSave == Boolean.FALSE && this.node != null){" +
-                    "return ((" + ctField.getType().getName() + ") this.node.getProperty(\"" + ctField.getName() + "\", null));" +
-                    "}else{" +
-                    "return " + ctField.getName() + ";" +
-                    "}" +
-                    "}";
-            addMethod(ctClass, entityName, code, getter);
-        }
-    }
+        // ~~~~~~~~~~~~~~~
+        // Adding cleanUp() method
+        //@formatter:off
+        String codeCleanUp = "public static void cleanUp() {" + 
+                                "return " + entityName + "._cleanUp(\"" + entityName + "\");" + 
+                             "}";
+        //@formatter:on
+        Logger.debug(codeCleanUp);
+        CtMethod cleanUpMethod = CtMethod.make(codeCleanUp, ctClass);
+        ctClass.addMethod(cleanUpMethod);
 
+        // ~~~~~~~~~~~~~~~
+        // Adding findAll() method
+        //@formatter:off
+        String codeFindAll = "public static java.util.List findAll() {" +
+                                "return " + entityName + "._findAll(\"" + entityName + "\");" +
+                             "}";
+        //@formatter:on
+        Logger.debug(codeFindAll);
+        CtMethod findAllMethod = CtMethod.make(codeFindAll, ctClass);
+        ctClass.addMethod(findAllMethod);
 
-    private void getterMethodForRelatedToVia(CtClass ctClass, String entityName, CtField ctField, String getter) throws NotFoundException, CannotCompileException {
-        try {
-            CtMethod ctMethod = ctClass.getDeclaredMethod(getter);
-            if (!ctMethod.getName().equalsIgnoreCase("getShouldBeSave")) {
-                ctClass.removeMethod(ctMethod);
-                throw new NotFoundException("it's not a true getter !");
-            }
-        } catch (NotFoundException noGetter) {
+        // Done.
+        applicationClass.enhancedByteCode = ctClass.toBytecode();
+        ctClass.defrost();
 
-            String code = "public " + ctField.getType().getName() + " " + getter + "() {" +
-                    "if(this." + ctField.getName() + " == null){" +
-                    "return getIterator(\"" + entityName + "\");" +
-                    "}else{" +
-                    "return " + ctField.getName() + ";" +
-                    "}" +
-                    "}";
-            addMethod(ctClass, entityName, code, getter);
-        }
-    }
-
-    private void getByKeyMethod(CtClass ctClass, String entityName) throws CannotCompileException {
-        String codeGetByKey = "public static play.modules.neo4j.model.Neo4jModel getByKey(Long key)  throws play.modules.neo4j.exception.Neo4jException  {" +
-                "return (" + entityName + ")_getByKey(key, \"" + entityName + "\");" +
-                "}";
-        createMethod(ctClass, entityName, codeGetByKey, "getByKey");
-    }
-
-    private void findAllMethod(CtClass ctClass, String entityName) throws CannotCompileException {
-        String code = "public static java.util.List findAll() {" +
-                "return " + entityName + "._findAll(\"" + entityName + "\");" +
-                "}";
-        createMethod(ctClass, entityName, code, "findAll");
-    }
-
-    private void cleanUpMethod(CtClass ctClass, String entityName) throws CannotCompileException {
-        String code = "public static void cleanUp() {" +
-                "return " + entityName + "._cleanUp(\"" + entityName + "\");" +
-                "}";
-        createMethod(ctClass, entityName, code, "cleanUp");
     }
 
     /**
-     * A javabean property must be public, not static and not final
-     * Ignore : Transient, RelatedTo and RelatedToVia field if they are annotated
+     * Is this field a valid javabean property ?
      */
-    private boolean isProperty(CtField ctField) throws ClassNotFoundException {
+    private boolean isProperty(CtField ctField) {
         if (ctField.getName().equals(ctField.getName().toUpperCase())
                 || ctField.getName().substring(0, 1).equals(ctField.getName().substring(0, 1).toUpperCase())) {
             return false;
         }
-        for (Object info : ctField.getAvailableAnnotations()) {
-            String annotationName = info.toString();
-            if (annotationName.startsWith("@javax.persistence.Transient") ||
-                    annotationName.startsWith("@play.modules.neo4j.annotation.RelatedTo") ||
-                    annotationName.startsWith("@play.modules.neo4j.annotation.RelatedToVia")
-                    ) {
-                return false;
-            }
-
+        if (hasNeo4jRelatedAnnotation(ctField)) {
+            return false;
         }
         return Modifier.isPublic(ctField.getModifiers()) && !Modifier.isFinal(ctField.getModifiers())
                 && !Modifier.isStatic(ctField.getModifiers());
-    }
-
-    private boolean isAnnotatedWithRelatedToVia(CtField ctField) throws ClassNotFoundException {
-        for (Object info : ctField.getAvailableAnnotations()) {
-            String annotationName = info.toString();
-            if (annotationName.startsWith("@play.modules.neo4j.annotation.RelatedToVia")) {
-                return true;
-            }
-
-        }
-        return false;
     }
 
     /**
@@ -268,16 +204,33 @@ public class Neo4jModelEnhancer extends Neo4jEnhancer {
     private boolean hasPlayPropertiesAccessorAnnotation(CtMethod ctMethod) {
         for (Object object : ctMethod.getAvailableAnnotations()) {
             Annotation ann = (Annotation) object;
-            //Logger.debug("Annotation method is " + ann.annotationType().getName());
+            Logger.debug("Annotation method is " + ann.annotationType().getName());
             if (ann.annotationType().getName()
                     .equals("play.classloading.enhancers.PropertiesEnhancer$PlayPropertyAccessor")) {
-                //Logger.debug("Method " + ctMethod.getName() + " has be enhanced by propertiesEnhancer");
+                Logger.debug("Method " + ctMethod.getName() + " has be enhanced by propertiesEnhancer");
                 return true;
             }
         }
-        //Logger.debug("Method " + ctMethod.getName() + " has not be enhance by propertiesEnhancer");
+        Logger.debug("Method " + ctMethod.getName() + " has not be enhance by propertiesEnhancer");
         return false;
     }
 
+    /**
+     * Is filed has neo4j related annotation ?
+     * 
+     * @param ctField
+     * @return
+     */
+    private boolean hasNeo4jRelatedAnnotation(CtField ctField) {
+        for (Object info : ctField.getAvailableAnnotations()) {
+            String annotationName = info.toString();
+            if (annotationName.startsWith("@play.modules.neo4j.annotation.RelatedTo")
+                    || annotationName.startsWith("@play.modules.neo4j.annotation.RelatedToVia")) {
+                return true;
+            }
+
+        }
+        return true;
+    }
 
 }
